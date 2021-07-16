93c3cb18f8b49eb3346848f14be9961fd5b28ba3
haeseung-lee <dlgotmd93@gmail.com>
2021-07-17 02:33:02 +0900

    0717 덧셈표만들기, 정사각형 출력 완성

--- myalgorithm/src/a_pageCheck/PageCheck.java
+++ myalgorithm/src/a_pageCheck/PageCheck.java
@@ -1,7 +1,7 @@
 package a_pageCheck;
 
 public class PageCheck {
-	//p37 차례.
+	//p39 차례.(곱셈표 완성, 덧셈표, 정사각형 출력 완료 - 다음으로 넘어가기)
 	public static void main(String[] args) {
 //		System.out.println("hello world");
 	}
