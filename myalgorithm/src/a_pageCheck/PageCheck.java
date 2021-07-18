942cc435d3f42853083360cc9bf5109234fcfee0
haeseung-lee <dlgotmd93@gmail.com>
2021-07-19 02:54:02 +0900

    0719..n단의 피라미드를 출력하는 메서드 작성

--- myalgorithm/src/a_pageCheck/PageCheck.java
+++ myalgorithm/src/a_pageCheck/PageCheck.java
@@ -1,7 +1,7 @@
 package a_pageCheck;
 
 public class PageCheck {
-	//p39 차례.(곱셈표 완성, 덧셈표, 정사각형 출력 완료 - 다음으로 넘어가기)
+	//p41 차례.(직각 이등변 삼각형 출력 완료, Q16.완료)
 	public static void main(String[] args) {
 //		System.out.println("hello world");
 	}
---
+++ myalgorithm/src/chap01/basic/Pyramid.java
@@ -0,0 +1,32 @@
+package chap01.basic;
+
+import java.util.Scanner;
+
+public class Pyramid {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("정수를 입력하세요");
+		int num = sc.nextInt();
+		spira(num);
+	}
+	
+	public static void spira(int n) {
+		
+		for(int i = 1; i <= n; i++) {
+			
+			for(int j = 0; j < n-i; j++) {
+				System.out.print(" ");
+			}
+			for(int k = 0; k < (i-1)*2 +1; k++) {
+				System.out.print("*");
+			}
+			for(int l = 0; l < n-i; l++) {
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+}
