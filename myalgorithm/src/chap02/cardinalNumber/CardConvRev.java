a585b6db304b5de3a9b851de08726098d7d77ab4
haeseung-lee <dlgotmd93@gmail.com>
2021-08-06 01:51:02 +0900

    0806 commit. cardinalNumber2..

--- myalgorithm/src/chap02/cardinalNumber/CardConvRev.java
+++ myalgorithm/src/chap02/cardinalNumber/CardConvRev.java
@@ -3,5 +3,16 @@ package chap02.cardinalNumber;
 //입력받은 10진수를 2진수 ~ 36진ㄴ수로 기수 변환하여 나타냄
 public class CardConvRev {
 
-	
+	//정수값 x를 r진수로 변환하여 배열 d에 아랫자리부터 넣어두고 자릿수를 반환합니다.
+	public static int cardConvR(int x, int r, char[] d) {
+		
+		int digits = 0;
+		String dchar = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		
+		do {
+			d[digits++] = dchar.charAt(x % r); //r로 나눈 나머지를 저장
+			x /= r;
+		} while (x != 0);
+		return digits;
+	}
 }
