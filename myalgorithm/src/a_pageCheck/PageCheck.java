107844570228605f272a765ffe8c31607503ad17
haeseung-lee <dlgotmd93@gmail.com>
2021-08-05 13:37:47 +0900

    0805..기수변환 예제

--- myalgorithm/src/a_pageCheck/PageCheck.java
+++ myalgorithm/src/a_pageCheck/PageCheck.java
@@ -1,7 +1,7 @@
 package a_pageCheck;
 
 public class PageCheck {
-	//p65 Q5 rcopy작업완료
+	//p67 Q5 rcopy작업완료
 	
 	public static void main(String[] args) {
 //		System.out.println("hello world");
---
+++ myalgorithm/src/chap02/cardinalNumber/CardConvRev.java
@@ -0,0 +1,7 @@
+package chap02.cardinalNumber;
+
+//입력받은 10진수를 2진수 ~ 36진ㄴ수로 기수 변환하여 나타냄
+public class CardConvRev {
+
+	
+}
