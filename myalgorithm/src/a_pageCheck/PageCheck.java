9e76151b7a0fd8544d52eb8d08da5929d78880c2
haeseung-lee <dlgotmd93@gmail.com>
2021-08-13 23:25:09 +0900

    0813.. 문제풀이중

--- myalgorithm/src/a_pageCheck/PageCheck.java
+++ myalgorithm/src/a_pageCheck/PageCheck.java
@@ -1,7 +1,7 @@
 package a_pageCheck;
 
 public class PageCheck {
-	//p67 Q5 rcopy작업완료
+	//p71 Q6 풀이중
 	
 	public static void main(String[] args) {
 //		System.out.println("hello world");
---
+++ myalgorithm/src/chap02/cardinalNumber/CardConv.java
@@ -0,0 +1,10 @@
+package chap02.cardinalNumber;
+
+//배열의 앞쪽에 아랫자리가 아니라 윗자리를 넣어두는 메서드 작성하기
+public class CardConv {
+	
+	public static int cardConv(int x, int r, char[] d) {
+		return 0;
+	}
+
+}
